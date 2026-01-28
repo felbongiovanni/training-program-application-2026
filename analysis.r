b5802dafd5b578aae097cbd34c90d5f2b5e92d9c
Felicia Bongiovanni <bongiovanni.f@wehi.edu.au>
2026-01-28 14:56:21 +1100

    Completed all instructions given in analysis.R code and saved final plot into the results folder

--- analysis.r
+++ analysis.r
@@ -19,12 +19,14 @@
 # Load libraries -------------------
 # You may use base R or tidyverse for this exercise
 
-# ex. library(tidyverse)
+library(tidyverse)
+library(here)
 
 # Load data here ----------------------
 # Load each file with a meaningful variable name.
 
-
+genetic_metadata <- read.csv(here::here("data/GSE60450_filtered_metadata.csv"))
+normalised_gene_expression <- read.csv(here::here("data/GSE60450_GeneLevel_Normalized(CPM.and.TMM)_data.csv"))
 
 # Inspect the data -------------------------
 
@@ -32,21 +34,41 @@
 # Keep the code here for each file.
 
 ## Expression data
-
+dim(normalised_gene_expression) # output: 23735 rows and 14 columns
 
 ## Metadata
-
+dim(genetic_metadata) # output: 12 rows and 4 columns
 
 # Prepare/combine the data for plotting ------------------------
 # How can you combine this data into one data.frame?
-
+combined_data <- normalised_gene_expression %>% 
+  pivot_longer(cols = starts_with("GSM")) %>% # transform data into long format (appropriate for plotting)
+  merge(genetic_metadata %>% # merge data based on GSM
+          rename("name" = "X"), 
+        by = "name")
 
 
 # Plot the data --------------------------
 ## Plot the expression by cell type
 ## Can use boxplot() or geom_boxplot() in ggplot2
-
+combined_data %>% 
+  ggplot() + 
+  geom_boxplot(aes(x = immunophenotype, 
+                   y = value,
+                   fill = developmental.stage)) + # further analyse data by splitting into developmental stage
+  scale_y_log10() + # transform gene expression data to log10 scale to visualise data better
+  xlab("Cell type") +
+  ylab("Normalised gene expression (log10-transformed)") +
+  theme_bw() + # adjust aesthetics to ease visualisation
+  theme(axis.text=element_text(size=15), # increase font sizes and remove grid lines
+        axis.title=element_text(size=16), 
+        strip.text = element_text(size = 15), 
+        legend.text=element_text(size=14), 
+        legend.title=element_text(size=15), 
+        panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank())
 
 
 ## Save the plot
 ### Show code for saving the plot with ggsave() or a similar function
+ggsave(here::here("results/gene_expression_by_cell_type.pdf"), dpi = 300)
